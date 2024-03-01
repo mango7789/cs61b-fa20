7dd9d22a0bf872c4db4343042848561204766ce1
mango7789 <115400861+mango7789@users.noreply.github.com>
2024-03-01 14:41:11 +0800

    update

--- lab3/IntDList/BuggyIntDList.java
+++ lab3/IntDList/BuggyIntDList.java
@@ -38,12 +38,12 @@ public class BuggyIntDList extends IntDList {
         // ------------------------------------------------------------------
 
         if (d1.val <= d2.val) {
-            d1.next = sortedMerge(d1, d2.next);   // FIXME: Replace this line (if needed). HINT: Step Into(F7) using debugger and try to figure out what it does.
+            d1.next = sortedMerge(d1.next, d2);   // FIXME: Replace this line (if needed). HINT: Step Into(F7) using debugger and try to figure out what it does.
             d1.next.prev = d1;
             d1.prev = null;
             return d1;
         } else {
-            d2.next = sortedMerge(d1.next, d2);   // FIXME: Replace this line (if needed). HINT: Step Into(F7) using debugger and try to figure out what it does.
+            d2.next = sortedMerge(d1, d2.next);   // FIXME: Replace this line (if needed). HINT: Step Into(F7) using debugger and try to figure out what it does.
             d2.next.prev = d2;
             d2.prev = null;
             return d2;
@@ -66,15 +66,15 @@ public class BuggyIntDList extends IntDList {
             temp = p.prev;
             p.prev = p.next;
             p.next = temp;
-            p = p.next;        // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
+            p = p.prev;        // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
         }
 
         // HINT: What does this if block do? Use Debugger and Java Visualizer to figure out.
         if (temp != null) {
             // ------ WRITE ADDITIONAL CODE HERE AND ONLY HERE (IF NEEDED) -----
-
+            temp.prev.next = temp; // front.next is not null
             // -----------------------------------------------------------------
-            front = temp.next;    // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
+            front = temp.prev;    // FIXME: Replace this line (if needed). HINT: Use debugger and Java Visualizer to figure out what it does.
         }
     }
 }

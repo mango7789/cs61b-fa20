60d44a27cb4b143619268296136f91e61704bec4
mango7789 <115400861+mango7789@users.noreply.github.com>
2024-03-13 20:48:06 +0800

    update

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,43 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    int N;
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.N = N;
+    };
+
+    private void check_index(int row, int col) {
+        if (row < 0 || row > N - 1 || col < 0 || col > N - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public void open(int row, int col) {
+        check_index(row, col);
+    };
+
+    public boolean isOpen(int row, int col) {
+        check_index(row, col);
+
+    };
+
+    public boolean isFull(int row, int col) {
+        check_index(row, col);
+
+    };
+
+    public int numberOfOpenSites() {
+
+    };
+
+    public boolean percolates() {
+
+    };
+
+    public static void main(String[] args) {
+
+    };
 }

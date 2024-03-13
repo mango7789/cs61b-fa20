54f82bb46b89222662b83c401f4c85c7ed85c4aa
mango7789 <115400861+mango7789@users.noreply.github.com>
2024-03-13 15:59:51 +0800

    edit

--- hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -1,4 +1,5 @@
 package es.datastructur.synthesizer;
+
 import java.util.Iterator;
 
 //TODO: Make sure to that this class and all of its methods are public
@@ -19,7 +20,9 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
-        first = 0; last = 0; fillCount = 0;
+        first = 0;
+        last = 0;
+        fillCount = 0;
         this.capacity = capacity;
         rb = (T[]) new Object[capacity];
     }
@@ -28,17 +31,13 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      * Adds x to the end of the ring buffer. If there is no room, then
      * throw new RuntimeException("Ring buffer overflow").
      */
+    @Override
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update
-        //       last. Don't worry about throwing the RuntimeException until you
-        //       get to task 4.
+        // last. Don't worry about throwing the RuntimeException until you
+        // get to task 4.
         rb[last] = x;
-        if (last == capacity - 1) {
-            last = 0;
-        } else {
-            last++;
-        }
-        
+        last = (last + 1) % capacity;
         fillCount += 1;
         return;
     }
@@ -47,16 +46,13 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      * Dequeue oldest item in the ring buffer. If the buffer is empty, then
      * throw new RuntimeException("Ring buffer underflow").
      */
+    @Override
     public T dequeue() {
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and
-        //       update first. Don't worry about throwing the RuntimeException until you
-        //       get to task 4.
+        // update first. Don't worry about throwing the RuntimeException until you
+        // get to task 4.
         T first_item = rb[first];
-        if (first == capacity - 1) {
-            first = 0;
-        } else {
-            first++;
-        }
+        first = (first + 1) % capacity;
         fillCount--;
         return first_item;
     }
@@ -65,15 +61,39 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      * Return oldest item, but don't remove it. If the buffer is empty, then
      * throw new RuntimeException("Ring buffer underflow").
      */
+    @Override
     public T peek() {
         // TODO: Return the first item. None of your instance variables should
-        //       change. Don't worry about throwing the RuntimeException until you
-        //       get to task 4.
+        // change. Don't worry about throwing the RuntimeException until you
+        // get to task 4.
         return rb[first];
     }
 
-
     // TODO: When you get to part 4, implement the needed code to support
-    //       iteration and equals.
+    // iteration and equals. 
+    private class ArrayRingBufferIterator implements Iterator<T> {
+        private int pos;
+        private int curNum;
+        public ArrayRingBufferIterator() {
+            pos = first;
+            curNum = 0;
+        }
+
+        public boolean hasNext() {
+            return curNum < fillCount;
+        }
+
+        public T next() {
+            T retValue = rb[pos];
+            pos = (pos + 1) % capacity;
+            curNum++;
+            return retValue;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator();
+    }
 }
-    // TODO: Remove all comments that say TODO when you're done.
+// TODO: Remove all comments that say TODO when you're done.
--- hw1/es/datastructur/synthesizer/BoundedQueue.java
+++ hw1/es/datastructur/synthesizer/BoundedQueue.java
@@ -1,15 +1,24 @@
 package es.datastructur.synthesizer;
 
+import java.util.Iterator;
+
 public interface BoundedQueue<T> {
     int capacity();
+
     int fillCount();
+
     void enqueue(T x);
+
     T dequeue();
+
     T peek();
+
+    Iterator<T> iterator();
 
     default boolean isEmpty() {
         return fillCount() == 0;
     }
+
     default boolean isFull() {
         return fillCount() == capacity();
     }

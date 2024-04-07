b455cc6ec63792b9698494272ae7dd5bccbd8981
mango7789 <115400861+mango7789@users.noreply.github.com>
2024-04-07 09:56:19 +0800

    proj3: world generation

--- proj3/byow/Core/WorldGenerator.java
+++ proj3/byow/Core/WorldGenerator.java
@@ -7,89 +7,87 @@ import byow.TileEngine.Tileset;
 import java.util.*;
 
 public class WorldGenerator {
-    private static final int GRID_WIDTH = 50;
-    private static final int GRID_HEIGHT = 50;
-
-    private static final TETile FLOOR = Tileset.FLOOR;
-    private static final TETile WALL = Tileset.WALL;
-    private static final TETile UNUSED = Tileset.NOTHING;
-
-    private TETile[][] world;
+    private int width;
+    private int height;
+    private TETile[][] worldGrid;
     private Random random;
 
-    public WorldGenerator() {
-        world = new TETile[GRID_HEIGHT][GRID_WIDTH];
-        random = new Random();
+    public WorldGenerator(int width, int height, long seed) {
+        this.width = width;
+        this.height = height;
+        this.worldGrid = new TETile[height][width];
+        this.random = new Random();
     }
 
     public void generateWorld() {
-        // Initialize grid with unused spaces
-        fillGridWithUnused();
-        // Generate rooms and hallways recursively
-        divide(1, 1, GRID_WIDTH - 2, GRID_HEIGHT - 2);
+        initialize();
+        // Generate rooms
+        int numRooms = random.nextInt(10) + 10; // Random number of rooms between 3 and 7
+        for (int i = 0; i < numRooms; i++) {
+            int roomWidth = random.nextInt(8) + 4; // Random room width between 4 and 11
+            int roomHeight = random.nextInt(6) + 4; // Random room height between 4 and 9
+            int x = random.nextInt(width - roomWidth);
+            int y = random.nextInt(height - roomHeight);
+            createRoom(x, y, roomWidth, roomHeight);
+        }
+
+        // Generate hallways
+        int numHallways = random.nextInt(3) + 2; // Random number of hallways between 2 and 4
+        for (int i = 0; i < numHallways; i++) {
+            int startX = random.nextInt(width);
+            int startY = random.nextInt(height);
+            int length = random.nextInt(10) + 5; // Random hallway length between 5 and 14
+            createHallway(startX, startY, length);
+        }
     }
 
-    private void fillGridWithUnused() {
-        for (int i = 0; i < GRID_HEIGHT; i++) {
-            for (int j = 0; j < GRID_WIDTH; j++) {
-                world[i][j] = UNUSED;
+    private void initialize() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                worldGrid[i][j] = Tileset.NOTHING;
             }
         }
     }
 
-    private void divide(int x1, int y1, int x2, int y2) {
-        if (x2 - x1 < 3 || y2 - y1 < 3) {
-            return;
-        }
-
-        // Choose whether to divide horizontally or vertically
-        boolean divideHorizontally = random.nextBoolean();
-
-        // Divide the space with a wall
-        int wx = divideHorizontally ? x1 : (random.nextInt((x2 - x1 - 1) / 2) * 2 + x1 + 1);
-        int wy = divideHorizontally ? (random.nextInt((y2 - y1 - 1) / 2) * 2 + y1 + 1) : y1;
-
-        // Draw the wall
-        if (divideHorizontally) {
-            for (int i = x1; i <= x2; i++) {
-                world[wy][i] = WALL;
-            }
-        } else {
-            for (int i = y1; i <= y2; i++) {
-                world[i][wx] = WALL;
+    private void createRoom(int x, int y, int width, int height) {
+        for (int i = y; i < y + height; i++) {
+            for (int j = x; j < x + width; j++) {
+                if (i == y || i == y + height - 1 || j == x || j == x + width - 1) {
+                    worldGrid[i][j] = Tileset.WALL; // Wall
+                } else {
+                    worldGrid[i][j] = Tileset.FLOOR; // Floor
+                }
             }
         }
-
-        // Create gaps in the wall
-        int gapX = divideHorizontally ? (random.nextInt((x2 - x1) / 2) * 2 + x1) : wx;
-        int gapY = divideHorizontally ? wy : (random.nextInt((y2 - y1) / 2) * 2 + y1);
-
-        world[gapY][gapX] = FLOOR;
-
-        // Recursively divide the four quadrants
-        divide(x1, y1, wx - 1, wy - 1);
-        divide(wx + 1, wy + 1, x2, y2);
-        divide(x1, wy + 1, wx - 1, y2);
-        divide(wx + 1, y1, x2, wy - 1);
     }
 
-    private void printWorld() {
-        for (int i = 0; i < GRID_HEIGHT; i++) {
-            for (int j = 0; j < GRID_WIDTH; j++) {
-                System.out.print(world[i][j]);
+    private void createHallway(int startX, int startY, int length) {
+        int direction = random.nextInt(4); // 0: up, 1: right, 2: down, 3: left
+        int x = startX;
+        int y = startY;
+        for (int i = 0; i < length; i++) {
+            if (x >= 0 && x < width && y >= 0 && y < height) {
+                worldGrid[y][x] = Tileset.FLOOR; // Floor
+                switch (direction) {
+                    case 0: y--; break;
+                    case 1: x++; break;
+                    case 2: y++; break;
+                    case 3: x--; break;
+                }
+            } else {
+                break;
             }
-            System.out.println();
         }
     }
 
     private void renderWorld() {
         TERenderer ter = new TERenderer();
-        ter.initialize(GRID_WIDTH, GRID_HEIGHT);
-        ter.renderFrame(world);
+        ter.initialize(width, height);
+        ter.renderFrame(worldGrid);
     }
 
     public static void main(String[] args) {
-        WorldGenerator worldGenerator = new WorldGenerator();
+        WorldGenerator worldGenerator = new WorldGenerator(100, 40, 1L);
         worldGenerator.generateWorld();
         worldGenerator.renderWorld();
     }
